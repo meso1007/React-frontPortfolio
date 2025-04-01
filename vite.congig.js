ff59dea8403768ddb7b28394e6f6f324ff177fd8
meso1007 <diegoshoya2017@gmail.com>
2025-04-02 01:02:19 +0900

    change vite config

--- vite.congig.js
+++ vite.congig.js
@@ -1,6 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-})
+  publicDir: 'public', // publicフォルダを指定
+  build: {
+    outDir: 'dist'
+  }
+});
